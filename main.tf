aae95cc8862705f2aeff0cab174dfad629223414
MoustafaDanash <123922676+MoustafaDanash@users.noreply.github.com>
2023-12-07 11:36:43 +0200

    2nd Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 
 resource "aws_instance" "myec2" {
   ami           = "ami-06d4b7182ac3480fa"
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
   tags = {
-    Name = "sentinel_check"
+    Name = "sentinal_check"
   }
 }
 
